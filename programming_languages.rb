bf17c248e7f08f47c2119ceb593b08d15c45d076
Nicholas Anaya <anaya.nicholas@gmail.com>
2017-01-31 19:13:37 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
+require "pry"
+require "pry-nav"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    languages[style].each do |language_name, value_hash|
+      if new_hash[language_name] == nil
+        new_hash[language_name] = value_hash
+        new_hash[language_name][:style] = [style]
+      else
+        new_hash[language_name][:style] << style
+      end
+    end
+  end
+  new_hash
 end
+
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
